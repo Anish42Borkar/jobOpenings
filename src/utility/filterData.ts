58bc40b90bc2cf0575848f30b39be7f1e2c6e104
Anish42Borkar <anishborkar42@gmail.com>
2023-01-08 12:54:12 +0530

    solve internet off isssue

--- src/utility/filterData.ts
+++ src/utility/filterData.ts
@@ -1,8 +1,9 @@
-import { ResponseData } from "../pages/jobOpenings/types";
+import { FilteredList, ResponseData } from "../pages/jobOpenings/types";
 
-export function filterData(data: ResponseData[]) {
+export function filterData(data: ResponseData[] | [] = []) {
   let filteredData: any = {};
-  data.forEach((val) => {
+
+  data?.map((val) => {
     if (val.department == null) {
       if (!filteredData.hasOwnProperty("Others")) {
         filteredData["Others"] = [];
